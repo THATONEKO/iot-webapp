eea9f92ea1dc003c13358431a9de82d69699d0e9
Thato Neko <thatoneko03@gmail.com>
2024-05-30 10:58:26 +0200

    app using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
+  imports:[
+    CommonModule
+  ],
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myy-app';
+export class AppComponent implements OnInit {
+  devices: any[] = [];
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  title = 'angular-waziup';
+
+  fetchData() {
+    console.log("Fetching devices...")
+    fetch('https://api.waziup.io/api/v2/devices?q=owner==thatoneko03@gmail.com')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.devices = data;
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
 }
